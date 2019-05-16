7cab109d9765524e115854b23058b9de8fa9bc3f
dongliang yu <ydlover@gmail.com>
2019-05-17 03:00:47 +0800

    build

--- build.sh
+++ build.sh
@@ -1,5 +1,4 @@
 export TAG=1.0 
-go build -ldflags "-X main._VERSION_='$TAG'" 
 
 gox -osarch="windows/amd64"
 gox -osarch="linux/386"
@@ -7,3 +6,4 @@ gox -osarch="linux/amd64"
 gox -osarch="linux/arm"
 gox -osarch="linux/arm64"
 gox -osarch="windows/386"
+zip -r release_${TAG}.zip . -x "./config/*" -x "./server-release.jar" -x "*.go" -x "./logs/*" -x ".git/*" -x ".gitignore"
